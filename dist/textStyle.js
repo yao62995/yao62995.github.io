43660d4d646c73731928853f71fd34361bc2d7b3
yaojian <yaojian.bd@bytedance.com>
2020-03-10 12:48:44 +0800

    new origin commit

---
+++ dist/textStyle.js
@@ -0,0 +1,15 @@
+
+var StyleLibrary = {
+    style_earthy_yellow: {
+        font: 'serif',
+        align: 'center',
+        color: 'black',
+        size: 18,
+        background: '#b8a388',
+        stroke: 1,
+        strokeColor: 'rgba(0, 0, 0, 1)'
+    }
+};
+
+
+
